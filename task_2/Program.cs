51adc0d0d57f61cd8c022af5d48493053a1f18b8
“Dmitriy” <colnce1-1@ya.ru>
2022-07-21 22:10:56 +0300

    add programm cube table

---
+++ task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Введите N: ");
+double N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+
+while (count <= N){
+   Console.WriteLine(Math.Pow(count,3));
+   count++;
+}
